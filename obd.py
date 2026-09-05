848dc7dd38718d1830639e17b4b782a52d8c7ae4
fuchse-ohren <git@kitsune.aleeas.com>
2026-09-05 17:23:39 +0900

    デコード関数を実装．データを継続的に取得し続けるように変更

--- obd.py
+++ obd.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from poplib import POP3_SSL_PORT
 from typing import Optional
 
 import can
@@ -14,14 +15,21 @@ import time
 
 @dataclass
 class CanConfig:
-    port: str = "COM5"
+    """
+    Slcanポートとの接続設定
+
+    Parameters
+    ----------
+     - port (str) = "COM6" - COMポートの名称
+     - bitrate (int) = 500000 - シリアル通信のビットレート
+    """
+    port: str = "COM6"
     bitrate: int = 500000
 
 
 class CanTransport:
     """
     CANable 2.0 + SLCAN を利用したCANトランスポート。
-    OBD/ISO-TPの知識を持たない。
     """
 
     def __init__(self, config: CanConfig):
@@ -69,21 +77,48 @@ class CanTransport:
 # ISO-TP Layer
 # ============================================================
 
+class IsotpError(Exception):
+    pass
+
 @dataclass
 class IsoTpConfig:
-    tx_id: int
-    rx_id: int
+    """
+    ISO-TP関連のパラメタを保持するクラス
+
+    Parameters
+    -----------
+     - tx_id (int) = 0x7DF - 送信用CAN ID
+     - rx_id (int) = 0x7E8 - 受信用CAN ID
+     - stmin (int) = 0 - フロー制御メッセージで送信される最小間隔時間。
+        連続する2つのフレーム間の待機時間を示す。この値はCANプロトコル上でそのまま送信される。
+        値が1から127の場合はミリ秒単位、0xF1から0xF9の場合は100マイクロ秒から900マイクロ秒の範囲を意味する。
+        値が0の場合はタイミング要件がないことを示す。
+     - blocksize (int) = 8 - フロー制御メッセージを送信する際にレイヤーが含める単一バイトのブロックサイズ。
+        送信者がフロー制御メッセージを要求する前に送信すべき連続フレーム数を表す。
+        値が 0 の場合、ブロックサイズは無限大（つまりフロー制御メッセージは送信されない）を意味する。
+     - wtfmax (int) = 0 - フロー制御メッセージに含める単一バイトの「待機フレーム最大値」を設定する。
+        この値が上限に達すると、受信処理は停止し、MaximumWaitFrameReachedError が発生する。
+        値が 0 の場合、待機フレームはサポートされておらず、送信も行われない。
+     - tx_data_length (int) = 8 - リンク層(CANレイヤー)が転送できる最大バイト数。
+        つまり、単一のCANメッセージで可能な限り最大のデータバイト数。
+        有効な値は : 8、12、16、20、24、32、48、64です。
+        大きなIsoTPフレームは、パディングを使用しない限り、可能な限り小さくなる最後のCANメッセージを除き、
+        このサイズの小型CANメッセージで送信されます。
+    """
+
+    tx_id: int = 0x7DF
+    rx_id: int = 0x7E8
 
     # ISO-TP parameters
     stmin: int = 0
-    blocksize: int = 0
+    blocksize: int = 8
     wftmax: int = 0
     tx_data_length: int = 8
 
 
 class IsoTpTransport:
     """
-    ISO 15765-2 transport layer。
+    ISO 15765-2 transport 層。
     """
 
     def __init__(
@@ -111,10 +146,9 @@ class IsoTpTransport:
             "blocksize": self.config.blocksize,
             "wftmax": self.config.wftmax,
             "tx_data_length": self.config.tx_data_length,
-            "rx_flowcontrol_timeout": 1000,
-            "rx_consecutive_frame_timeout": 1000,
-            "tx_padding": 0x00,
-            #"rx_padding": 0x00,
+            "rx_flowcontrol_timeout": 100,
+            "rx_consecutive_frame_timeout": 100,
+            "tx_padding": 0x00
         }
 
         self.stack = isotp.CanStack(
@@ -126,13 +160,14 @@ class IsoTpTransport:
     def send_and_receive(
         self,
         payload: bytes,
-        timeout: float = 5.0,
+        timeout: float = 0.5,
     ) -> bytes:
 
         if self.stack is None:
-            raise RuntimeError("ISO-TP stack is not open")
+            raise RuntimeError("ISO-TPスタックの開設に失敗しました")
 
         self.stack.send(payload)
+        print("ISOTP ↑\t",payload.hex(" "))
 
         deadline = __import__("time").monotonic() + timeout
 
@@ -140,11 +175,15 @@ class IsoTpTransport:
             self.stack.process()
 
             if self.stack.available():
-                return self.stack.recv()
+                res = self.stack.recv()
+                if res == None:
+                    raise IsotpError("ISO-TPで正常な応答を受信できませんでした")
+                print("ISOTP ↓\t",bytes(res).hex(" "))
+                return bytes(res)
 
             __import__("time").sleep(0.001)
 
-        raise TimeoutError("ISO-TP response timed out")
+        raise TimeoutError("ISO-TP応答がタイムアウトしました")
 
 
 # ============================================================
@@ -174,16 +213,6 @@ class ObdNegativeResponse(ObdError):
 class ObdClient:
     """
     OBD-IIサービス層。
-
-    現在:
-        Mode 09 PID 02 -> VIN
-
-    将来:
-        Mode 01 PID
-        Mode 03
-        Mode 04
-        Mode 09 その他PID
-        等を追加可能。
     """
 
     def __init__(
@@ -191,6 +220,44 @@ class ObdClient:
         transport: IsoTpTransport,
     ):
         self.transport = transport
+        self.dec_fomula = {
+            0x04: lambda d: int(d[0]) / 2.55, # 発動機負荷(%)
+            0x05: lambda d: int(d[0]) - 40, # 冷却水温(C)
+            0x0C: lambda d: (256 * int(d[0]) + int(d[1])) / 4, # 発動機回転数(rpm)
+            0x0d: lambda d: int(d[0]), # 車速(km/h)
+            0x0F: lambda d: int(d[0]) - 40, # 吸気温度(C)
+            0x10: lambda d: (256 * int(d[0]) + int(d[1])) / 100, # 空気流量(g)
+            0x2F: lambda d: int(d[0]) / 2.55, # 燃料残量(%)
+            0x43: lambda d: ( 256 * int(d[0]) + int(d[1]) ) / 2.55, # 絶対負荷(%)
+            0x44: lambda d: (256 * int(d[0]) + int(d[0])) / 32768, # 空燃比等量比指令(λ)
+            0x46: lambda d: int(d[0]) - 40, # 外気温(C)
+            0x48: lambda d: int(d[0]) / 2.55, # スロットル位置C(%)
+            0x49: lambda d: int(d[0]) / 2.55, # スロットル位置D(%)
+            0x4B: lambda d: int(d[0]) / 2.55, # スロットル位置F(%)
+            0x4C: lambda d: int(d[0]) / 2.55, # スロットルアクチュエーター指令(%)
+        }
+        self.supported_pids = self.dec_fomula.keys()
+
+
+    def decoder(self, pid: int, data: bytes):
+        """
+        PID応答をデコードする
+
+        Parameters
+        ----------
+         - pid (int) - OBD-IIのパラメタID
+         - data (bytes) - 受信したバイナリデータ
+
+        Exceptions
+        -----------
+         - ObdError - パラメタIDが不正だった場合の例外
+        """
+
+        try:
+            return self.dec_fomula[pid](data)
+        except KeyError as e :
+            raise ObdError("デコードできないパラメタIDが指定されました")
+
 
     def request(self, service: int, pid: int) -> bytes:
 
@@ -281,7 +348,7 @@ def main() -> None:
 
     can_transport = CanTransport(
         CanConfig(
-            port="COM6",
+            port="COM4",
             bitrate=500000,
         )
     )
@@ -306,15 +373,23 @@ def main() -> None:
     try:
         can_transport.open()
         iso_tp.open()
-
-        vin = obd.get_vin()
-
-        print(f"VIN = {vin}")
-
-        obd.get_supported()
-
-    finally:
+    except:
         can_transport.close()
+        raise Exception("ポート初期化に失敗")
+
+    while True:
+        time.sleep(0.1)
+        try:
+            for pid in obd.supported_pids:
+                res_bin = obd.request(0x01,pid)
+                res = obd.decoder(pid,res_bin)
+                print("pid:",pid,"\tres:",res)
+        except KeyboardInterrupt as e:
+            can_transport.close()
+            return
+        except Exception as e:
+            print(e)
+            pass
 
 
 if __name__ == "__main__":
